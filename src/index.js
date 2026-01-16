051d8331c54d3251ea9b80cc6e2ad316679488f2
Michael mello <dakton2001@Gmail.com>
2026-01-16 20:11:29 -0300

    Add SQLite support and update .gitignore for database
    Added 'better-sqlite3' and 'sqlite3' dependencies for SQLite support. Updated .gitignore to exclude swap files and the new database directory. Modified configuration and service files to integrate SQLite functionality.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,49 @@
+/** 
+ * @description streamchatrelay application main bootstrap file
+ * @author Michael Mello - drakomichael in github
+ * @since 2025-12-20
+ * @lastUpdate 2026-01-16
+ * @version 1.0.0
+ **/
+
+/**
+ * @main bootstrap file
+ * Initializes web server, WebSocket connections, and database.
+ * Loads configuration and settings and insert in modules.
+ */
+
+// Web server modules import - express server for API and web interface 
+//import express_bootstrap from "./services/webManager/express_bootstrap.js";
+
+// WebSocket modules import - webSocket server for real time data
 import websocket_bootstrap from "./services/webSocket/websocket_bootstrap.js";
-import config from "./config.json" with { type: "json" };
-import express_bootstrap from "./services/webManager/express_bootstrap.js";
-import startUtilities from "./services/etc/startUtilities.js";
+
+// Config import
+//import config from "./config.json" with { type: "json" };
+//import startUtilities from "./services/etc/startUtilities.js";
 import loadSettings from "./services/etc/loadsettings.js";
+
+// External connections import - twitch WS connection
 import TwitchConnectionWS from "./services/externalConnections/twitch/connectionWS.js";
+
+// Database import - sqlite3 bootstrap in memory
+import sqlite3_bootstrap from "./services/dataBase/sqlite3_bootstrap.js";
+import sqlite3_bootstrap_memory from "./services/dataBase/sqlite3_bootstrap_memory.js";
 
 
 class main{
     async bootstrap(){
-        const settings = loadSettings(config); // try load conf, invalid -> load backup
+
+        // Load settings and check ambient type (prod/dev)
+        const settings = await loadSettings(); // try, invalid -> load backup
+
         await websocket_bootstrap(settings);
-
-        if(config.use_webserver){
-            await express_bootstrap(settings);
-        }
-
-        if(config.debbug){ // use in specific cases for debbuging twitch WS or like this
-            const twitchWS = new TwitchConnectionWS();
-            twitchWS.connect();
-        }
-
-        //startUtilities(settings); // Call utils in etc if in dev mode
+        await sqlite3_bootstrap();
+        await sqlite3_bootstrap_memory();
     };   
 };
  
 const app = new main()
 
 ;app.bootstrap();
-// this ; it's a semicolon to prevent possible ASI issues - idk just my mania
+// this ; to prevent possible ASI issues - idk just my mania 
--- src/services/etc/loadsettings.js
+++ src/services/etc/loadsettings.js
@@ -1,7 +1,15 @@
 import config_backup from "./config_backup.json" with { type: "json" };
 import liveChatSpam from "../spamGenerator/liveChatSpam.js"
+import startUtilities from "./startUtilities.js";
+import express_bootstrap from "../webManager/express_bootstrap.js";
+import TwitchConnectionWS from "../externalConnections/twitch/connectionWS.js";
+import sqlite3_bootstrap from "../dataBase/sqlite3_bootstrap.js";
+import sqlite3_bootstrap_memory from "../dataBase/sqlite3_bootstrap_memory.js";
 
-export default function loadSettings(config) {
+//pick up config from main folder
+import config from "../../config.json" with { type: "json" };
+
+export default async function loadSettings() {
   if (!config || !config.type_ambience || Object.keys(config).length === 0 ) {
     console.log("Config is missing or invalid. Loading backup configuration.");
     return config_backup;
@@ -9,6 +17,22 @@ export default function loadSettings(config) {
   if(config.type_ambience === "dev") {
     console.log("Development ambience detected. Applying development settings.");
     liveChatSpam();
+    startUtilities(config);
+  }
+  if(config.use_webserver){
+    await express_bootstrap(config);
+  }
+  if(config.debbug){ // use in specific cases for debbuging twitch WS or like this
+    const twitchWS = new TwitchConnectionWS();
+    twitchWS.connect();
+  }
+  if(config.database.enable_database){
+    if(config.database.enable_in_disk_db){
+      sqlite3_bootstrap();
+    }
+    if(config.database.enable_in_memory_db){
+      await sqlite3_bootstrap_memory();
+    }
   }
 
   return config;
